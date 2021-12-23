7263510ea51c385c7d9cc28b7666fad8d0d8df12
Savindu Harith <sharitecdigitallabs@gmail.com>
2021-12-23 10:44:35 +0530

    Remove unnecessary console.log call

--- public/js/app.js
+++ public/js/app.js
@@ -1,8 +1,3 @@
-console.log('Client side js file is loaded');
-
-
-    
-
 const weaterForm = document.querySelector('form');
 const search = document.querySelector('input');
 const messageOne = document.querySelector('#message-1');
